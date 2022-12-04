229939c87821bd25499cdaa02a3565a52e45f4fa
Tanmay Maheshwari <tm19534cse2019@gmail.com>
2022-12-04 22:37:36 +0530

    adds functionality to sort, search college by name and state

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const csv = require("csv");
+const fs = require("fs");
+
+var colleges = [];
+
+fs.readFile("./data/engineering colleges in India.csv", (err, data) => {
+    csv.parse(data, function (err, data) {
+        data.shift(); //remove first element from array that is usless data
+        //console.log(data);
+        data.sort(function (a, b) {
+            return a[0].localeCompare(b[0]);
+        });
+        colleges = data;
+    });
+});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/colleges/total", (req, res) => {
+    const str = {
+        total: colleges.length,
+    };
+    res.send(JSON.stringify(str));
+});
+
+app.get("/colleges/state", (req, res) => {
+    if (req.query.name) {
+        const name = req.query.name.toLowerCase();
+        const result = [];
+        for (let i = 0; i < colleges.length; i++) {
+            if (colleges[i][11].toLowerCase().indexOf(name) >= 0) {
+                colleges[i][0] = colleges[i][0].replace(/\:[^>]*\)/gi, "");
+                result.push({
+                    college_name: colleges[i][0],
+                    city: colleges[i][10],
+                    state: colleges[i][11],
+                });
+                if (result.length === 10) {
+                    break;
+                }
+            }
+        }
+        //result.push(colleges.find((el) => el[0].includes(name)));
+        res.send(JSON.stringify(result));
+    }
+});
+
+app.get("/colleges", (req, res) => {
+    //res.send(JSON.stringify(colleges));
+    //console.log("req rec");
+    //console.log(req.query);
+
+    if (req.query.name) {
+        const name = req.query.name.toLowerCase();
+        const result = [];
+        for (let i = 0; i < colleges.length; i++) {
+            if (colleges[i][0].toLowerCase().indexOf(name) >= 0) {
+                colleges[i][0] = colleges[i][0].replace(/\:[^>]*\)/gi, "");
+                result.push({
+                    college_name: colleges[i][0],
+                    city: colleges[i][10],
+                    state: colleges[i][11],
+                });
+                if (result.length === 10) {
+                    break;
+                }
+            }
+        }
+        //result.push(colleges.find((el) => el[0].includes(name)));
+        res.send(JSON.stringify(result));
+    }
+});
+
+app.listen(PORT);
